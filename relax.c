ead22bd3bce9b5fe3d1eaf748a42c9171cb1be36
ankrt <ankrt@mail.com>
2014-12-02 16:55:13 +0000

    About to fix indexing

--- relax.c
+++ relax.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <mpi.h>
 #include <math.h>
+#include <mpi.h>
+
+// displacements and chunk size
+struct scatter_spec {
+        int *displs, *allocation;
+};
 
 /*
- * Generate array and fill with random numbers
+ * generate_array
+ * generate an array and fill with random numbers
  * TODO: choose whether seed is on or off
  */
 double* generate_array(int length, int randwanted)
@@ -20,7 +26,8 @@ double* generate_array(int length, int randwanted)
 }
 
 /*
- * Print array to stdout in square form
+ * print_array
+ * print to stdout in square form
  */
 void print_array(double *array, int size)
 {
@@ -37,15 +44,55 @@ void print_array(double *array, int size)
 }
 
 /*
- * Initialisation that the root process does
- * at the start of the program
+ * get_ranges
+ * given the size of the array and the number of processors
+ * work out the row ranges that each processor will be sent
  */
-void root_pretask(int argc, char **argv)
+struct scatter_spec* get_scatter_spec(int size, int numprocs)
+{
+        int innersize = size - 2;
+        int chunksize = innersize / numprocs;
+        int remainder = innersize % numprocs;
+        int i, p;
+        struct scatter_spec *sspc = malloc(sizeof(struct scatter_spec));
+
+        int *allocation = malloc(numprocs * sizeof(int));
+        allocation[0] += size;
+        /*allocation[numprocs] += size;*/
+        for (i = 0; i < numprocs; i++) {
+                allocation[i] = chunksize * size;
+        }
+        p = 0;
+        while (remainder > 0) {
+                allocation[p] += size;
+                remainder--;
+                p++;
+                if (p == numprocs) p = 0;
+        }
+
+        int *dsp = malloc(numprocs * sizeof(int));
+        for (i = 0; i < numprocs; i++) {
+                if (i == 0) {
+                        dsp[i] = 0;
+                } else {
+                        dsp[i] = dsp[i - 1] + allocation[i - 1];
+                }
+        }
+
+        sspc->displs = dsp;
+        sspc->allocation = allocation;
+        return sspc;
+}
+
+/*
+ * root_pretask
+ * root process must do a few extra things before starting
+ */
+void root_pretask(int argc, char **argv, int numprocs)
 {
         double *array;
-        int size,
-            arraylen,
-            precision;
+        int size, arraylen, precision;
+        struct scatter_spec *sspc;
 
         // handle arguments
         if (argc != 3) {
@@ -57,10 +104,23 @@ void root_pretask(int argc, char **argv)
                 arraylen = size * size;
         }
 
-        // generate array and fill with numbers
         array = generate_array(arraylen, 1);
+        // get the displacements to pass to the scatter function
+        sspc = get_scatter_spec(size, numprocs);
         print_array(array, size);
+
+        int i;
+        for (i = 0; i < numprocs; i++) {
+                printf("proc %d starts at %d, using %d elements\n",
+                                i,
+                                sspc->displs[i],
+                                sspc->allocation[i]);
+        }
+
         free(array);
+        free(sspc->displs);
+        free(sspc->allocation);
+        free(sspc);
 }
 
 int main(int argc, char **argv)
@@ -79,17 +139,13 @@ int main(int argc, char **argv)
         MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
         MPI_Get_processor_name(name, &namelen);
 
-        printf("numprocs: %d, my rank: %d, running on: %s\n",
-                        numprocs,
-                        myrank,
-                        name);
+        /*printf("numprocs: %d, my rank: %d, running on: %s\n",*/
+                        /*numprocs,*/
+                        /*myrank,*/
+                        /*name);*/
 
-        // root process needs to do extra initialisation
-        //      - create array
-        //      - partition array
-        //      - scatter array
         if (myrank == 0) {
-                root_pretask(argc, argv);
+                root_pretask(argc, argv, numprocs);
         }
         // enter loop, calculating averages
         // gather up pieces of array
